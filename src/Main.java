670c509040c21d984c9a1cfa99d9a442bb8f398c
Luke MacLean <40969439+MacLeanLuke@users.noreply.github.com>
2021-09-21 23:49:35 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        NumberInWord.printNumberInWord(1);
+        NumberInWord.printNumberInWord(-1);
+        NumberInWord.printNumberInWord(2);
+        NumberInWord.printNumberInWord(6);
+        NumberInWord.printNumberInWord(4);
+        NumberInWord.printNumberInWord(10);
+        NumberInWord.printNumberInWord(15);
+        NumberInWord.printNumberInWord(0);
+        NumberInWord.printNumberInWord(-5);
+        NumberInWord.printNumberInWord(10);
+    }
+}
